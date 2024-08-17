6e78b959e40930dfde429ba637cdcbe98f1816a6
joodmohaxx <joodased@gmail.com>
2024-08-17 18:01:17 +0300

    Updated assignment_1

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,16 @@
 def players_as_dictionaries(squads_list):
-    pass
+    users = []
+    for user in squads_list:
+        user1 = {
+            'number': user[0],
+            'position': user[1],
+            'name': user[2],
+            'date_of_birth': user[3],
+            'caps': user[4],
+            'club': user[5],
+            'country': user[6],
+            'club_country': user[7],
+            'year': user[8]
+        }
+        users.append(user1)
+    return users
