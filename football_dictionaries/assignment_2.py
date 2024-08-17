11093a9b77ac22138cd8b43c1f0e0d6fdbc5e877
joodmohaxx <joodased@gmail.com>
2024-08-17 17:49:59 +0300

    Update assignment_2.

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,10 @@
 def players_by_position(squads_list):
-    pass
+    players_by_position = {}
+
+    for player in players:
+        position = player['position']
+        if position not in players_by_position:
+            players_by_position[position] = []
+        players_by_position[position].append(player)
+
+    return players_by_position
