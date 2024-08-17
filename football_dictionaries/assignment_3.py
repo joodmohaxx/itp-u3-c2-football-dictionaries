5789d277f1394ca193ae442675e792a1c06b883c
joodmohaxx <joodased@gmail.com>
2024-08-17 17:45:20 +0300

    Update assignment_3.

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,11 @@
 def players_by_country_and_position(squads_list):
-    pass
+  
+    player_country = {}
+
+    for player in players:
+        country = player["country"]
+        position = player["position"]
+        player_country.setdefault(country, {})
+        player_country[country].setdefault(position, [])
+        player_country[country][position].append(player)
+    return player_country
